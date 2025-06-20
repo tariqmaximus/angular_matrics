06bd993e877afa582ef1148bce6194804ef2953e
tariqmaximus <tariq.maximus@gmail.com>
2025-06-20 05:35:07 -0700

    first commit

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NgMoringaTableModule } from 'ng-moringa-table';
 
+
 @Component({
   selector: 'app-root',
   imports: [NgMoringaTableModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   tableData = [
